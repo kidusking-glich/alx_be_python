e193c1eecc4547017cfd282b6d50ae9796feef76
kidusking-glich <abelyohannes69@gmail.com>
2025-09-13 17:37:13 +0300

    Alx project creating foldes and files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+a = number1 + number2
+b = number1 - number2
+c = number1 * number2
+print("Addition of 10 and 5 is ",a)
+print("Subtraction of 10 and 5 is",b)
+print("Multiplication of 10 and 5 is ", c)
