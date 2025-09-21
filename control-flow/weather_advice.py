03bfddb12b712750ffa30c04394a2536fa181acc
kidusking-glich <abelyohannes69@gmail.com>
2025-09-21 15:09:50 +0300

    Fixing Issue

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,8 +1,5 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-"""sun = "sunny" 
-rain = "rainy"
-cold = "cold"""
 
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
@@ -11,4 +8,4 @@ elif weather == "rainy":
 elif weather == "cold" :
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Wrong Selection")
+    print("Sorry, I don't have recommendations for this weather.")
